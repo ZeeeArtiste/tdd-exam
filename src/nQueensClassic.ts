05aacb05cfa5051e9df0abb7fd1163862c7191ea
Dany <dany@MacBook-Pro-de-Dany.local>
2025-02-12 14:51:59 +0100

    handle function to pass test for 2n - no solution

--- src/nQueensClassic.ts
+++ src/nQueensClassic.ts
@@ -2,6 +2,12 @@ export function solveNQueensClassic(n: number): string[][] {
     if (n === 1) {
       return [["#"]];
     }
+
+    // No solution for n=2
+    if (n === 2) {
+        return [];
+      }
+
     return [];
   }
   
